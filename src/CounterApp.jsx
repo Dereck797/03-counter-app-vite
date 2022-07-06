15a7bd84ee20ffd6a9170311d911a9cbe926885a
Dereck Sebastián Muñoz Castiblanco <dsmunozc@indracompany.com>
2022-07-05 22:18:19 -0500

    Ultimo cambio

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => { 
-    const event = (event) => {
-        console.log(event);
-        // El valor de Value no se muestra en pantalla por que no se se está renderizando
-        value = 1000;   
-        console.log(value);
+
+    // Entre parentesis se define el valor inicial del hook
+    const [counter, setCounter] = useState( value );
+
+
+    const event = () => {
+        // setCounter(counter + 1);
+        // lo anterior también se puede hacer de la siguiente manera
+        // 'co' es el valor del estado anterior de counter
+        setCounter((co) => co + 1);
     }
 
     return (
         <>
         <h1>CounterApp</h1>
-        <h2>{ value }</h2>
+        <h2>{ counter }</h2>
 
         <button onClick={event} > +1 </button>
         </>
