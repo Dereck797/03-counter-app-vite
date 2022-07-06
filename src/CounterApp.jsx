d735305e9e71f9b5d344eaa5eff0366e41ca3415
Dereck Sebastián Muñoz Castiblanco <dsmunozc@indracompany.com>
2022-07-05 20:29:01 -0500

    44 Event Click

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,12 +1,21 @@
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({value}) => {
-  return (
-    <>
-      <h1>CounterApp</h1>
-      <h2>{ value }</h2>
-    </>
-  )
+export const CounterApp = ({value}) => { 
+    const event = (event) => {
+        console.log(event);
+        // El valor de Value no se muestra en pantalla por que no se se está renderizando
+        value = 1000;   
+        console.log(value);
+    }
+
+    return (
+        <>
+        <h1>CounterApp</h1>
+        <h2>{ value }</h2>
+
+        <button onClick={event} > +1 </button>
+        </>
+    )
 }
 
 
